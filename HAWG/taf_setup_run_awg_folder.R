6c2b7520381648d0154a2646f43a95d91b935881
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2026-03-04 21:33:14 +0100

    Adding square list to boot

---
+++ HAWG/taf_setup_run_awg_folder.R
@@ -0,0 +1,51 @@
+
+library(icesTAF)
+
+
+# taf.skeleton()
+
+# getwd()
+# 
+# setwd("./HAWG")
+# 
+# getwd()
+
+# Boot  ----
+mkdir("boot/initial/data/template")
+
+draft.data(data.files = "template",
+           data.scripts = NULL,
+           originator = "Data call and SC's",
+           title = "Copy of HAWG templates",
+           file = T,
+           access = "Public",
+           append = F)
+
+mkdir("boot/initial/data/template_submitted_last_year")
+
+draft.data(data.files = "template_submitted_last_year",
+           data.scripts = NULL,
+           originator = "DTU Aqua",
+           title = "Template with DNK data submitted last year",
+           file = T,
+           access = "Restricted",
+           append = T)
+
+
+mkdir("boot/initial/data/template_submitted_last_year")
+
+draft.data(data.files = "template_submitted_last_year",
+           data.scripts = NULL,
+           originator = "DTU Aqua",
+           title = "Template with DNK data submitted last year",
+           file = T,
+           access = "Restricted",
+           append = T)
+
+taf.boot()
+
+# mkdir("data")
+# mkdir("data_scripts")
+# mkdir("model/WKSAND16")
+
+
