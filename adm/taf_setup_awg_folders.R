384cee807907cce73ca335bb5146d7c7806de5d2
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2026-03-04 12:20:09 +0100

    Update taf_setup_awg_folders.R

--- adm/taf_setup_awg_folders.R
+++ adm/taf_setup_awg_folders.R
@@ -3,26 +3,27 @@
 
 library(icesTAF)
 
-# library(remotes)
-# install_github("ices-tools-dev/RDBEScore", build_vignettes = TRUE
-
 getwd()
-# setwd("./kibi_one_repo")
-# getwd()
 
-# Create AWG folders
-## Based on a file created from Annex 1, where DK | 'All countries' is mentioned
+setwd("C:/Users/kibi/OneDrive - Danmarks Tekniske Universitet/2026_all_RDBES_DK")
 
-annex_1 <- openxlsx::read.xlsx(
-  "Q:/mynd/Assessement_discard_and_the_like/national_AWG_BM_submissions/adm/2024/ices_big_data_call_annex_1_overview_2024_2024-03-05.xlsx"
-)
+dat <- c()
 
-awgs <- unique(annex_1$Expert.Group)
+for (i in c(2:16)) {
+  
+  dat_0 <- readxl::read_xlsx(paste0(".adm/boot/data/data_call/30939734/", "DC_Annex_1.xlsx"), sheet = c(i))
+  
+  dat <- rbind(dat, dat_0)
+}
+
+names(dat) <- gsub(" ", "", names(dat))
+
+awgs <- unique(dat$ExpertGroup)
 
 for (i in awgs) {
   
   mkdir(i)
-  icesTAF::taf.skeleton(path = paste0("./", i))
+  taf.skeleton(path = paste0("./", i))
   
   # Adding a utilities script
   file.create(paste0("./", i, "/", "utilities.R"))
@@ -33,22 +34,6 @@ for (i in awgs) {
   for (j in folder_types) {
     mkdir(paste0("./", i, "/", j, "_scripts"))
   }
- 
+  
 }
 
-# setwd("./AFWG")
-getwd()
-icesTAF::draft.data()
-
-draft.data(
-  data.files = "data_from_rdbes",
-  data.scripts = NULL,
-  originator = "HAWG",
-  year = "2025",
-  title = "Data from the RDBES summed",
-  access     = "Restricted",
-  file = T,
-  append = F
-)
-
-icesTAF::taf.boot()
